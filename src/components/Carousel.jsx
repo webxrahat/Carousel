b1750a4a24318609ffddcfb595e2ceaece39e97b
Md Rahat Islam <islammdrahat308@gmail.com>
2024-12-11 21:06:50 +0600

    tg

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -15,7 +15,12 @@ const Carousel = () => {
     }}
     className="h-[50vh] w-full relative"
    >
-    <div className="absolute right-96 top-1/4">
+    <div className="absolute right-96 top-5">
+     <div>
+      <img src={img1} className="h-72 w-52 rounded-lg border object-cover" />
+     </div>
+    </div>
+    <div className="absolute right-80 top-5">
      <div>
       <img src={img1} className="h-72 w-52 rounded-lg border object-cover" />
      </div>
