a2aaadec711b24d162a8a892f050855a81c96f2b
Md Rahat Islam <islammdrahat308@gmail.com>
2024-12-11 07:57:23 +0600

    image customization

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,11 +1,25 @@
 import React from "react";
-import img1 from "../assets/img_1.avif";
+import img1 from "../assets/img_1.jpg";
+
+// linear-gradient(to bottom, #87CEEB 10%, #FFFFFF 100%),
 
 const Carousel = () => {
  return (
   <div>
-   <div className="h-96 bg-yellow-800">
-    <img src={img1} />
+   <div
+    style={{
+     backgroundImage: `url(${img1})`,
+     backgroundSize: "cover",
+     backgroundRepeat: " no-repeat",
+     backgroundPosition: "center",
+    }}
+    className="h-[50vh] w-full relative"
+   >
+    <div className="absolute right-96 top-1/4">
+     <div>
+      <img src={img1} className="h-72 w-52 rounded-lg border object-cover" />
+     </div>
+    </div>
    </div>
   </div>
  );
